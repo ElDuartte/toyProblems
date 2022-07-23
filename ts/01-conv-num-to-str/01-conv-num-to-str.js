d8c0176e043e7b0736cd55969082524d01995a18
juan <jf@duartte.co>
2022-07-23 20:47:50 +0200

    hello world

---
+++ ts/01-conv-num-to-str/01-conv-num-to-str.js
@@ -0,0 +1,14 @@
+function numberToString(num) {
+    //return a string of the number here!
+    // const str = String(num);
+    // return str;
+    if (num > 0) {
+        var hello = "hello";
+        return hello;
+    }
+    else {
+        var no = "no";
+        return no;
+    }
+}
+numberToString(2);
