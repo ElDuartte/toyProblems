72a39b3d35f932a095b4fe7c8f97f3c4ef594e28
juan <jf@duartte.co>
2022-07-23 12:48:12 +0200

    yes

--- ts/01-conv-num-to-str/01-conv-num-to-str.ts
+++ ts/01-conv-num-to-str/01-conv-num-to-str.ts
@@ -1,7 +1,14 @@
-function numberToString(num: number): string{
-    //return a string of the number here!
-  const str = String(num);
-  return str;
+function numberToString(num: number): string {
+  //return a string of the number here!
+  // const str = String(num);
+  // return str;
+  if (num > 0) {
+    let hello: string = "hello";
+    return hello;
+  } else {
+    let no: string = "no";
+    return no;
+  }
 }
 
-module.export = numberToString;
+numberToString(2); 
